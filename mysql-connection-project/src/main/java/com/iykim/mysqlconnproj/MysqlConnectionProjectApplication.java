068c833b701f9c08b6208632a5f458d9eb350a15
inyeobkim <inyeob4078@gmail.com>
2021-05-31 20:38:49 +0900

    initial commit

---
+++ mysql-connection-project/src/main/java/com/iykim/mysqlconnproj/MysqlConnectionProjectApplication.java
@@ -0,0 +1,13 @@
+package com.iykim.mysqlconnproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlConnectionProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlConnectionProjectApplication.class, args);
+	}
+
+}
---
+++ mysql-connection-project/src/test/java/com/iykim/mysqlconnproj/MysqlConnectionProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iykim.mysqlconnproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlConnectionProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
